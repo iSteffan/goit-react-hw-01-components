9a34733cc8b2ab0615ffa0c68201966167d4b85e
Stepan Skichko <st.skichko@gmail.com>
2023-04-07 15:15:05 +0300

    task 1 render DONE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from "./Profile/Profile";
+import userData from "../backend-data/user.json"
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile {...userData} />
     </div>
   );
 };
